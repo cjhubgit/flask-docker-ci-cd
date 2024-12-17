7d6aced3e46452db7e21101b2f8ff1b63538cf60
Ahmed Yusuf <githubahmed1@gmail.com>
2024-12-17 11:24:08 +0300

    Initial Flask app with Docker setup

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
 FROM python:3.11-slim
 
-WORKDIR /APP
 
-COPY requirements.txt /app/requirements.txt /APP/
+WORKDIR /app
+
+
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY app.py /app/app.py /app/
+
+COPY app.py .
 
 EXPOSE 8080
 
-CMD [ "python", "app.py" ]
+
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "Hello, World! This is Flask CI/CD with Docker and Render Cloud."
+    return "Hello, World! This is Flask CI/CD with Docker and Render Cloud."
 
-if __name__=="__main__:" 
-     app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":  # Corrected the colon placement
+    app.run(host="0.0.0.0", port=8080)
